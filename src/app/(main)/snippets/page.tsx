a6d9b4ad70bd76937f42244634a08c6b62798600
Ibrahim Fonyuy <ibrahimfonyuy06@gmail.com>
2025-09-19 14:50:46 +0000

    Expected 'JavaScript' | 'TypeScript' | 'Node.js' | 'Python' | 'Go' | 'Ja

--- src/app/(main)/snippets/page.tsx
+++ src/app/(main)/snippets/page.tsx
@@ -131,7 +131,7 @@ export default function CodeSnippetsPage() {
                 render={({ field }) => (
                    <FormItem>
                     <FormLabel>Language</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <Select onValueChange={field.onChange} value={field.value}>
                       <FormControl>
                         <SelectTrigger>
                           <SelectValue placeholder="Select a language" />
